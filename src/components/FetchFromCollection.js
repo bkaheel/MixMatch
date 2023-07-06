d0d2f474d573b88e41a4613abb94473339c5b664
Millie Kembe <mikembe@unc.edu>
2023-07-06 13:49:22 -0400

    bug fixes again

--- src/components/FetchFromCollection.js
+++ src/components/FetchFromCollection.js
@@ -1,5 +1,5 @@
 import { collection, query, where, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
+import { db } from "../Firebase";
 import React, { useState, useEffect } from 'react';
 
 export const FetchFromCollection = ( code ) => {
