c2623bba0821a62a53ef01bc0505b8cda761da37
Millie Kembe <mikembe@unc.edu>
2023-07-06 13:43:07 -0400

    bug fixes again

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import { auth } from "../Firebase";
 
 const Login = () => {
     const navigate = useNavigate();
