5457c404327e0102094f52f8c5d81c5c9906eeda
Millie Kembe <mikembe@unc.edu>
2023-07-07 16:11:19 -0400

    changed callback

--- src/components/RegisterSpotify.jsx
+++ src/components/RegisterSpotify.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const RegisterSpotify = () => {
 
-  const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=31a74c4a34d245d8bcc83e7bed21b650&response_type=code&redirect_uri=https://mix-match.vercel.app/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-top-read%20user-read-recently-played%20user-modify-playback-state"
+  const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=31a74c4a34d245d8bcc83e7bed21b650&response_type=code&redirect_uri=https://gleeful-marzipan-4493fa.netlify.app/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-top-read%20user-read-recently-played%20user-modify-playback-state"
 
   return (
     <div>
