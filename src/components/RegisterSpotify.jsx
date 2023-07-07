353c1d8f24525fdb04df26296c5fddab293ae7b6
Millie Kembe <mikembe@unc.edu>
2023-07-06 23:33:08 -0400

    route vercel test

--- src/components/RegisterSpotify.jsx
+++ src/components/RegisterSpotify.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const RegisterSpotify = () => {
 
-  const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=31a74c4a34d245d8bcc83e7bed21b650&response_type=code&redirect_uri=http://localhost:5173/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-top-read%20user-read-recently-played%20user-modify-playback-state"
+  const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=31a74c4a34d245d8bcc83e7bed21b650&response_type=code&redirect_uri=https://mix-match-6mp58gqdl-mkembe.vercel.app/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-top-read%20user-read-recently-played%20user-modify-playback-state"
 
   return (
     <div>
