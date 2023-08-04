9116003262f427a46ef6dbb5443a04f2fd1dd29d
Millie Kembe <mikembe@unc.edu>
2023-08-03 21:41:35 -0400

    trying again with workflow

--- api/refresh.js
+++ api/refresh.js
@@ -1,14 +1,34 @@
 // refresh.js
 
-const { default: SpotifyWebApi } = require('spotify-web-api-node'); 
-const { collection, getDocs, updateDoc, doc } = require('firebase/firestore');
-const { db } = require('../src/Firebase');
+import SpotifyWebApi from 'spotify-web-api-node';
+import { getDocs, updateDoc, doc } from 'firebase/firestore';
 
-export default async (req, res) => {
+import { db } from '../src/Firebase';
 
-  const usersRef = collection(db, "accounts");
-  
-  const snapshot = await getDocs(usersRef);
+export default async function handler(req, res) {
+
+  try {
+
+    await refreshAllTokens();
+    
+    console.log('Tokens refreshed successfully');
+
+  } catch (error) {
+
+    console.error('Error refreshing tokens:', error);
+
+  }
+
+  setTimeout(handler, 3600000); // call again in 1 hour
+
+  return res.status(200).json({msg: 'Success'});
+
+}
+
+
+async function refreshAllTokens() {
+
+  const snapshot = await getDocs(collection(db, "accounts"));
 
   snapshot.forEach(async (userDoc) => {
 
@@ -19,17 +39,15 @@ export default async (req, res) => {
         redirectUri: "http://localhost:5173/callback",
         clientId: "31a74c4a34d245d8bcc83e7bed21b650",
         clientSecret: "9d292ba379844b4a8dc69ccfa60996aa",
-        refreshToken,
+        accessToken,
     });
 
     const data = await spotifyApi.refreshAccessToken();
-    
+
     await updateDoc(doc(db, "accounts", user.uid), {
       accessToken: data.body.access_token 
     });
 
   });
 
-  res.status(200).json({ message: 'Tokens refreshed!' });
-
-};
+}
