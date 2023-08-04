2ea1821281cfd1734bc1537d08d4c05c176079bd
Millie Kembe <mikembe@unc.edu>
2023-08-03 20:35:44 -0400

    testing

--- server/server.js
+++ server/server.js
@@ -89,7 +89,7 @@ app.post("/refresh", (req, res) => {
 app.post("/login", (req, res) => {
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
-      redirectUri: "http://localhost:5173/callback",
+      redirectUri: "https://mix-match-zvzf.vercel.app/callback",
       clientId: "31a74c4a34d245d8bcc83e7bed21b650",
       clientSecret: "9d292ba379844b4a8dc69ccfa60996aa",
     })
